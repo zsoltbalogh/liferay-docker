051de281bf93cb0b2a403a2a0f5f369212384f35
Zsolt Balogh <zsolt.balogh@liferay.com>
2020-10-21 12:38:05 +0000

    LPS-122428 Separate the OS image from base. The new Dockerfile lines are coming from https://hub.docker.com/r/azul/zulu-openjdk-alpine/dockerfile

--- build_os_image.sh
+++ build_os_image.sh
@@ -6,12 +6,12 @@ function build_docker_image {
 	local image_version=$(./release_notes.sh get-version)
 
 	DOCKER_IMAGE_TAGS=()
-	DOCKER_IMAGE_TAGS+=("liferay/base:${image_version}-${TIMESTAMP}")
-	DOCKER_IMAGE_TAGS+=("liferay/base")
+	DOCKER_IMAGE_TAGS+=("liferay/os:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("liferay/os")
 
 	docker build \
 		--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
-		--build-arg LABEL_NAME="Running environment for Liferay bundles - OS, JVM and tools" \
+		--build-arg LABEL_NAME="Operationg system for Liferay images" \
 		--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
 		--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
 		--build-arg LABEL_VERSION="${image_version}" \
@@ -20,9 +20,9 @@ function build_docker_image {
 }
 
 function main {
-	update_image base
+	update_image os
 
-	make_temp_directory templates/base
+	make_temp_directory templates/os
 
 	build_docker_image
 
