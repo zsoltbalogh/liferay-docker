383d12d0d4db89c1bb4017a5d61ffaf8939da116
Zsolt Balogh <zsolt.balogh@liferay.com>
2020-10-21 13:17:39 +0000

    LPS-122428 Copying base, no changes

---
+++ build_servlet_container_image.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./_common.sh
+
+function build_docker_image {
+	local image_version=$(./release_notes.sh get-version)
+
+	DOCKER_IMAGE_TAGS=()
+	DOCKER_IMAGE_TAGS+=("liferay/base:${image_version}-${TIMESTAMP}")
+	DOCKER_IMAGE_TAGS+=("liferay/base")
+
+	docker build \
+		--build-arg LABEL_BUILD_DATE=$(date "${CURRENT_DATE}" "+%Y-%m-%dT%H:%M:%SZ") \
+		--build-arg LABEL_NAME="Running environment for Liferay images - OS, JVM and tools" \
+		--build-arg LABEL_VCS_REF=$(git rev-parse HEAD) \
+		--build-arg LABEL_VCS_URL="https://github.com/liferay/liferay-docker" \
+		--build-arg LABEL_VERSION="${image_version}" \
+		$(get_docker_image_tags_args ${DOCKER_IMAGE_TAGS[@]}) \
+		${TEMP_DIR} || exit 1
+}
+
+function main {
+	update_image base
+
+	build_image os os || exit 1
+
+	make_temp_directory templates/base
+
+	build_docker_image
+
+	push_docker_images ${1}
+
+	clean_up_temp_directory
+}
+
+main ${@}
