0f17d5644d5f5624c415b3521574c6c719ab12e5
Zsolt Balogh <zsolt.balogh@liferay.com>
2021-09-06 20:21:18 +0200

    DOCKER-26 Adding change to support command parameter ordering, written by @Akos-Kreutz, recommitting in pieces

--- validation/beautysh.py
+++ validation/beautysh.py
@@ -22,11 +22,11 @@ FUNCTION_STYLE_REGEX = [
 
 FUNCTION_STYLE_REPLACEMENT = [r"function \g<1>() ", r"function \g<1> ", r"\g<1>() "]
 
+errors = {}
 
 def main():
     """Call the main function."""
     Beautify().main()
-
 
 class Beautify:
     """Class to handle both module and non-module calls."""
@@ -39,6 +39,10 @@ class Beautify:
         self.check_only = False
         self.apply_function_style = None  # default is no change based on function style
         self.color = True
+        self.argument_order = False
+        self.function_order = False
+        self.english = False
+        self.line_end = False
 
     def read_file(self, fp):
         """Read input file."""
@@ -275,28 +279,196 @@ class Beautify:
             sys.stderr.write("File %s: error: indent/outdent mismatch: %d.\n" % (path, tab))
         return "\n".join(output), error
 
+    def change_argument_order(self, data, path):
+        """Checks if the argument calls are in ABC order in the line. If not, then change the order."""
+        regexp = re.compile(r' -{1,2}[a-zA-Z]+')
+        black_list = "|,&;"
+
+        for line in data.split('\n'):
+            # If the line contains the [ character, then it's most likely an If statement, which could give us false positive
+            # eval lines are currently not supported
+            if not "[" in line and not "eval" in line and regexp.search(line): 
+                # Spliting the line into substrings by pipe or & characters
+                substrings = re.split(' [|&]{1,2} ',line)
+                # Saving the | and & characters for reconstruction
+                pipes = re.findall(' [|&]{1,2} ',line)
+                
+                for substring_index in range(len(substrings)):
+                    first_index = 0
+                    # Spliting the substring by spaces
+                    splits = substrings[substring_index].split(' ')
+                    splits_length = len(splits)
+                    arguments={}
+
+                    # If the substring matches the regex, it means that the string contains at least one argument
+                    if regexp.search(substrings[substring_index]):
+                        for j in range(1, splits_length):
+                            # If the split matches the regex, then it's an argument
+                            if regexp.search(" %s"%(splits[j])):
+                                # Sometimes there are function letters, which need to be addressed
+                                if(first_index == 0):
+                                    first_index = j
+                                # If the next split contains any of the blacklisted character or is an argument, then no value pair required
+                                if(j + 1 == splits_length or (splits[j + 1][:1] != '"' and splits[j + 1][:2] != "\\\"" and (any(c in black_list for c in splits[j + 1]) or regexp.search(" %s"%(splits[j + 1]))))):
+                                    arguments[splits[j]] = ""
+                                    # Increment the last index, so we know where was the last argument
+                                    last_index = j
+                                else:
+                                    arguments[splits[j]] = " %s"%(splits[j + 1])
+                                    # Increment the last index, so we know where was the last argument
+                                    last_index = j + 1    
+                        # The first split will always be the command/script call
+                        new_substring = splits[0]
+                        # Adding the function letters back to the beginning of the line
+                        for j in range(1, first_index):
+                            new_substring += " %s"%(splits[j])
+                        # Ordering the selected arguments by ABC and putting them in with their value pairs
+                        for ordered_agrument in sorted([re.sub(r'^-*', '', w) for w in arguments]):
+                            if "-%s"%(ordered_agrument) in arguments:
+                                ordered_agrument = "-%s"%(ordered_agrument)
+                            elif "--%s"%(ordered_agrument) in arguments:
+                                ordered_agrument = "--%s"%(ordered_agrument)
+                            new_substring += " %s%s"%(ordered_agrument,arguments[ordered_agrument])
+                        # The range between the last argument index and the length of the split is the non argument values
+                        for j in range(last_index + 1, len(splits)):
+                            new_substring += " %s"%(splits[j])         
+                    # Else the substring does not contain any argument, thus can be reconstructed without any modification                 
+                    else:
+                        new_substring = splits[0]
+                        for j in range(1, splits_length):
+                            new_substring += " %s"%(splits[j])  
+                    
+                    # If this is the first substring, then the new_line value will be overridden
+                    if substring_index == 0:
+                        new_line = new_substring
+                    # Else we concat it with the pipe and the new substring
+                    else:
+                        new_line += pipes[substring_index -1] + new_substring
+
+                if line != new_line:
+                    errors[path] += "The file contains argument order issue(s).\n"
+                        
+                # Replacing the old line with the new
+                data = data.replace(line, new_line)
+
+        return data
+
+    def change_function_order(self, data, path):
+        """Checks if the functions are in ABC order. If not, then change the order."""
+        regexp = re.compile(r'function.*')
+        if regexp.search(data):
+            lines = data.split('\n')
+            functions={}
+            line_number=0
+            start_line_number = -1
+
+            for line in lines:
+                # If the line matches the regex, then we consider it as a function start
+                if regexp.search(line):
+                    # Saving the function name and the line it's on
+                    start_line = line.replace(" {", "").replace("function ", "")
+                    start_line_number = line_number
+
+                # If there is already a function definition found and the line only consist of }, the script assumes that this is the end of the function
+                if start_line_number > -1 and line == "}":
+                    # Saving the function name (key) and the start and stop line numbers (value)
+                    functions[start_line] = "%s;%s"%(start_line_number,line_number)
+                    start_line_number = -1
+
+                line_number += 1
+            
+            new_data = ""
+            # Adding all the lines before the first function declaration to the new_data string.
+            for i in range(int(functions[list(functions)[0]].split(';')[0])):
+                new_data += "%s\n"%(lines[i])
+
+            # Adding the ordered functions to the new_data string.
+            for function in sorted(functions):
+                split = functions[function].split(';')
+                for i in range(int(split[0]), int(split[1]) + 1):
+                    new_data += "%s\n"%(lines[i])
+                new_data += "\n"
+
+            # Adding all the lines after the last function declaration to the new_data string.
+            for i in range(int(functions[list(functions)[len(functions) -1]].split(';')[1]) + 1, len(lines)):
+                new_data += "%s"%(lines[i])
+
+            if new_data != data:
+                errors[path] += "The file contains function order issue(s).\n"
+            return new_data
+        else:
+            return data
+
+    def check_last_line(self, data, path):
+        """Checks if the last line is empty"""
+        lines = data.split('\n')
+        changed = False
+
+        for i in range(len(lines) -1, 0, -1):
+            if lines[i] == "":
+                lines.pop(i)
+                changed = True
+            else:
+                break
+                       
+        if changed:
+            errors[path] += "The file contains empty last line(s)\n"
+
+        return "\n".join(lines)
+
     def beautify_file(self, path):
         """Beautify bash script file."""
         error = False
         if path == "-":
             data = sys.stdin.read()
             result, error = self.beautify_string(data, "(stdin)")
+
+            if self.argument_order:
+                result = self.change_argument_order(result, path)
+            if self.function_order and self.apply_function_style == 1:
+                result = self.change_function_order(result, path)
+            elif self.function_order and self.apply_function_style != 1:
+                errors[path] += "Function ordering does not support the provided function style.\n"
+                error = True
+            if self.line_end:
+                result = self.check_last_line(result, path)
             sys.stdout.write(result)
         else:  # named file
             data = self.read_file(path)
             result, error = self.beautify_string(data, path)
+
+            if self.argument_order:
+                result = self.change_argument_order(result, path)
+            if self.function_order and self.apply_function_style == 1:
+                result = self.change_function_order(result, path)
+            elif self.function_order and self.apply_function_style != 1:
+                errors[path] += "Function ordering does not support the provided function style.\n"
+                error = True
+            if self.line_end:
+                result = self.check_last_line(result, path)
             if data != result:
                 if self.check_only:
                     if not error:
                         # we want to return 0 (success) only if the given file is already
                         # well formatted:
                         error = result != data
-                        if error:
-                            self.print_diff(data, result)
+                        # print out the changes
                 else:
                     if self.backup:
                         self.write_file(path + ".bak", data)
                     self.write_file(path, result)
+        
+        if self.english:
+            try:
+                result.encode(encoding='utf-8').decode('ascii')
+            except UnicodeDecodeError:
+                errors[path] += "The file contains non English characters.\n"
+                error = True
+
+        if error:
+            print(Fore.CYAN + "%s\n"%(path) + Fore.RESET)
+            self.print_diff(data, result)
+
         return error
 
     def color_diff(self, diff):
@@ -396,6 +568,30 @@ class Beautify:
         )
         parser.add_argument("--help", "-h", action="store_true", help="Print this help message.")
         parser.add_argument(
+            "--argument-order",
+            "-a",
+            action="store_true",
+            help="Beautysh will reorder arguments to be in ABC order." " Eval lines currently not supported.",
+        )
+        parser.add_argument(
+            "--function-order",
+            "-f",
+            action="store_true",
+            help="Beautysh will reorder functions to be in ABC order." " Only fnonly functions are supported.",
+        )
+        parser.add_argument(
+            "--english",
+            "-e",
+            action="store_true",
+            help="Beautysh will check if the file contains non English characters.",
+        )
+        parser.add_argument(
+            "--line-end",
+            "-l",
+            action="store_true",
+            help="Beautysh will check if the file contains non English characters.",
+        )
+        parser.add_argument(
             "files",
             metavar="FILE",
             nargs="*",
@@ -418,6 +614,10 @@ class Beautify:
         self.tab_size = args.indent_size
         self.backup = args.backup
         self.check_only = args.check
+        self.argument_order = args.argument_order
+        self.function_order = args.function_order
+        self.english = args.english
+        self.line_end = args.line_end
         if args.tab:
             self.tab_size = 1
             self.tab_str = "\t"
@@ -430,7 +630,21 @@ class Beautify:
         if "NO_COLOR" in os.environ:
             self.color = False
         for path in args.files:
+            errors[path] = ""
             error |= self.beautify_file(path)
+
+        if  args.check:
+            print("\nSummary:")
+            if error:
+                for file in sorted(errors):
+                    if errors[file] != "":
+                        print(Fore.CYAN + "  %s:"%(file) + Fore.RESET)
+                        for file_error in errors[file].split('\n'):
+                            if file_error != "":
+                                print(Fore.RED + "    - %s"%(file_error) + Fore.RESET)
+            else:
+                print(Fore.GREEN + "No error found." + Fore.RESET)
+
         sys.exit((0, 1)[error])
 
 
