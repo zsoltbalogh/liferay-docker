e28e055cd7feab87df147986b56defbc5b38af7c
Zsolt Balogh <zsolt.balogh@liferay.com>
2022-08-08 10:11:36 +0200

    DOCKER-149 We need to configure the Chrome path to ensure Poshi finds it

--- templates/poshi/resources/usr/local/bin/liferay_poshi_entrypoint.sh
+++ templates/poshi/resources/usr/local/bin/liferay_poshi_entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+function configure_poshi {
+	echo "browser.chrome.bin.file=/usr/local/chrome-linux/chrome" >> /opt/liferay/poshi/${POSHI_ENVIRONMENT}/poshi-ext.properties
+}
+
 function main {
+	configure_poshi
+
+	run_poshi
+}
+
+function run_poshi {
 	cd /opt/liferay/poshi/${POSHI_ENVIRONMENT}
 
 	gradle runPoshi
