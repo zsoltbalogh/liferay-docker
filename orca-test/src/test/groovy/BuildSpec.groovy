2f03c4c35eb6e1b3e01ef85f13334955fa827625
Josef Sustacek <josef.sustacek@liferay.com>
2022-10-25 14:40:07 +0200

    Use YAML parser to verify docker-compose contents

--- orca-test/src/test/groovy/BuildSpec.groovy
+++ orca-test/src/test/groovy/BuildSpec.groovy
@@ -1,4 +1,4 @@
-import org.apache.commons.io.FileUtils
+import groovy.yaml.YamlSlurper
 
 import java.util.concurrent.TimeUnit
 
@@ -6,7 +6,8 @@ class BuildSpec extends BaseOrcaSpec {
 
     def "'build' should create docker-compose.yml"(int timeoutSeconds) {
         when:
-            def orcaRun = orca.startOrca('build', '1.0.0')
+            // build some non-default (non-1.0.0) version
+            def orcaRun = orca.startOrca('build', '2.1.42')
             orcaRun.waitFor(timeoutSeconds, TimeUnit.SECONDS)
 
         then:
@@ -16,30 +17,26 @@ class BuildSpec extends BaseOrcaSpec {
 
             stdout.contains('Using the default single server configuration.')
 
-            stdout.contains('Building antivirus')
-            stdout.contains('Building backup')
-            stdout.contains('Building db')
-            stdout.contains('Building liferay')
-            stdout.contains('Building log-proxy')
-            stdout.contains('Building log-server')
-            stdout.contains('Building search')
-            stdout.contains('Building web-server')
-
-            def dockerCompose = new File(orca.buildsPath(), '1.0.0/docker-compose.yml')
+            def dockerCompose = new File(orca.buildsPath(), '2.1.42/docker-compose.yml')
             dockerCompose.isFile()
 
-            // TODO use YAML parser?
-            dockerCompose.text.contains('services:')
-        
-            dockerCompose.text.contains('antivirus:')
-            dockerCompose.text.contains('backup:')
-            dockerCompose.text.contains('ci:')
-            dockerCompose.text.contains('db:')
-            dockerCompose.text.contains('liferay:')
-            dockerCompose.text.contains('log-proxy:')
-            dockerCompose.text.contains('log-server:')
-            dockerCompose.text.contains('search:')
-            dockerCompose.text.contains('web-server:')
+            // Parse YAML, confirm the content
+            def ys = new YamlSlurper()
+
+            def dockerComposeYaml = ys.parseText(dockerCompose.text)
+
+            dockerComposeYaml.services != null
+
+            dockerComposeYaml.services.antivirus?.image == 'antivirus:2.1.42'
+            dockerComposeYaml.services.backup?.image == 'backup:2.1.42'
+            dockerComposeYaml.services.ci?.image == 'ci:2.1.42'
+            dockerComposeYaml.services.db?.image == 'db:2.1.42'
+            dockerComposeYaml.services.liferay?.image == 'liferay:2.1.42'
+            dockerComposeYaml.services.'log-proxy'?.image == 'log-proxy:2.1.42'
+            dockerComposeYaml.services.'log-server'?.image == 'log-server:2.1.42'
+            dockerComposeYaml.services.search?.image == 'search:2.1.42'
+            dockerComposeYaml.services.vault?.image == 'vault:2.1.42'
+            dockerComposeYaml.services.'web-server'?.image == 'web-server:2.1.42'
 
         cleanup:
             dumpOrcaStdoutToFile(stdout)
