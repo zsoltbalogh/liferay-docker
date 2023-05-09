b7595c4b0561fac0332eefeb430bffed0bed896d
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-05-09 13:27:57 +0200

    SRE-3046 Improve teleport-agent's service management

--- orca/templates/teleport-proxy/resources/usr/local/bin/add-node.sh
+++ orca/templates/teleport-proxy/resources/usr/local/bin/add-node.sh
@@ -44,7 +44,7 @@ function main {
 
 function print_join_command {
 	echo "Run the following command on the node to join:"
-	echo "$ teleport start --roles=node --token=${INVITE_TOKEN} --ca-pin=${CA_PIN} --auth-server=${TELEPORT_AUTH_SERVER}"
+	echo "$ teleport start --roles=node --token=${INVITE_TOKEN} --ca-pin=${CA_PIN} --auth-server=$(egrep -o auth-server=[0-9\.]+ /etc/systemd/system/teleport.service | cut -f2 -d=)"
 }
 
 main
