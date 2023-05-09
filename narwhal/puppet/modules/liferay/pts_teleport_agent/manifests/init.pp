4655ffae0a46f82cb6cb663c9e5d026f5c21e3ef
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-05-09 10:18:45 +0200

    SRE-3046 Fix teleport.service

--- narwhal/puppet/modules/liferay/pts_teleport_agent/manifests/init.pp
+++ narwhal/puppet/modules/liferay/pts_teleport_agent/manifests/init.pp
@@ -20,12 +20,12 @@ class pts_teleport_agent {
 
 	file {
 		'/etc/systemd/system/teleport.service':
+			content => epp("${module_name}/etc/systemd/system/teleport.service.epp"),
 			group => root,
 			mode => '0664',
 			notify => Service['teleport'],
 			owner => root,
 			require => Package['teleport'],
-			source => "puppet:///modules/${module_name}/etc/systemd/system/teleport.service",
 	}
 
 	package {
