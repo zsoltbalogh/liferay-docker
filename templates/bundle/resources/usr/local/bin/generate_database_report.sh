dfa33ef4abc1260e3d4953b3c895200f02f9495e
Zsolt Balogh <zsolt.balogh@liferay.com>
2023-07-05 12:48:08 +0200

    SRE-2426 Rename to TOC

--- templates/bundle/resources/usr/local/bin/generate_database_report.sh
+++ templates/bundle/resources/usr/local/bin/generate_database_report.sh
@@ -9,7 +9,7 @@ function check_usage {
 
 	QUERY_FILE=$(mktemp)
 
-	CONTENT_FILE=$(mktemp)
+	TOC_FILE=$(mktemp)
 
 	REPORTS_FILE="${LIFERAY_REPORTS_DIRECTORY}"/database_query_report_$(date +'%Y-%m-%d_%H-%M-%S').html
 }
@@ -17,7 +17,7 @@ function check_usage {
 function main {
 	check_usage
 
-	echo "<h1>Table of contents</h1>" >> "${CONTENT_FILE}"
+	echo "<h1>Table of contents</h1>" >> "${TOC_FILE}"
 
 	lc_time_run run_query "${LCP_SECRET_DATABASE_NAME}" "SHOW ENGINE INNODB STATUS;"
 
@@ -38,15 +38,15 @@ function main {
 	sed -e "s#<TD>#<TD><PRE>#g" -i "${QUERY_FILE}"
 	sed -e "s#</TD>#</PRE></TD>#g" -i "${QUERY_FILE}"
 
-	cat "${CONTENT_FILE}" "${QUERY_FILE}" > "${REPORTS_FILE}"
+	cat "${TOC_FILE}" "${QUERY_FILE}" > "${REPORTS_FILE}"
 
-	rm "${CONTENT_FILE}" "${QUERY_FILE}"
+	rm "${TOC_FILE}" "${QUERY_FILE}"
 }
 
 function run_query {
 	ANCHOR_ID=$((ANCHOR_ID+1))
 
-	echo "<a href=\"#${ANCHOR_ID}\">${ANCHOR_ID}. ${2}</a><br />" >> "${CONTENT_FILE}"
+	echo "<a href=\"#${ANCHOR_ID}\">${ANCHOR_ID}. ${2}</a><br />" >> "${TOC_FILE}"
 
 	echo "<h1 id=\"${ANCHOR_ID}\">${2}</h1>" >> "${QUERY_FILE}"
 
