cd2f4545cb59f0adba7a738c8a914bd39dfb951c
Tamas Papp <tamas.papp@liferay.com>
2024-01-12 11:51:51 +0100

    LRP-4730 Generate .product_info.json

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source _liferay_common.sh
+source _product_json.sh
 source _promotion.sh
 source _publishing.sh
 
@@ -14,7 +15,8 @@ function check_usage {
 
 	lc_cd "$(dirname "$(readlink /proc/$$/fd/255 2>/dev/null)")"
 
-	_PROMOTION_DIR="${PWD}/release-data/promotion/files"
+	_RELEASE_ROOT_DIR="${PWD}"
+	_PROMOTION_DIR="${_RELEASE_ROOT_DIR}/release-data/promotion/files"
 
 	rm -fr "${_PROMOTION_DIR}"
 
@@ -46,6 +48,12 @@ function main {
 	lc_time_run upload_boms liferay-public-releases
 
 	lc_time_run copy_rc
+
+	lc_time_run get_file_product_info_json
+
+	lc_time_run get_file_release_properties
+
+	lc_time_run generate_product_info_json
 }
 
 function print_help {
